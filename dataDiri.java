ff929e914f3019e639890344423e51c6fd61bec6
Rijal666 <rizkirizalmualim2@gmail.com>
2023-03-08 20:47:03 +0700

    Biodata Diri

---
+++ dataDiri.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+   
+class BiodataMahasiswa {
+  public static void main(String args[]){
+            
+    System.out.println("##  Program Java Biodata Mahasiswa ##");
+    System.out.println("========================================");
+    System.out.println();
+    System.out.println("# Data Mahasiswa #");
+    System.out.println("==================");
+    System.out.println("Nama: Rizki Rizal Mualim");
+    System.out.println("NIM: 2022310023");
+    System.out.println("Fakultas: FI22B");
+    System.out.println("Jurusan: Teknik Informatika" );
+    System.out.println("Kota Asal: Bekasi");
+    System.out.println("Alamat: VGH Blok AS 8 No.12");
+  }
+}
